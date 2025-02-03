0d5f4032808d74ad5e59134a495d63e32021ae58
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2025-02-03 23:03:53 +0100

    Add base spdx license identifier code - wip

--- src/i18n_check/generate_map_object.py
+++ src/i18n_check/generate_map_object.py
@@ -11,6 +11,8 @@ import json
 import re
 from pathlib import Path
 
+SPDX_LICENSE_IDENTIFIER = "SPDX-License-Identifier: AGPL-3.0-or-later"
+
 # MARK: Paths / Files
 
 i18n_check_dir = Path(__file__).parent.parent.resolve()
@@ -72,7 +74,9 @@ i18n_map_dict = nest_flat_dict({k: k for k, _ in en_us_json_dict.items()})
 frontend_types_dir = (Path(__file__).parent.parent.parent / "types").resolve()
 
 with open(frontend_types_dir / "i18n-map.ts", encoding="utf-8", mode="w") as f:
-    f.write(f"export const i18nMap = {json.dumps(i18n_map_dict, indent=2)}")
+    f.write(
+        f"// {SPDX_LICENSE_IDENTIFIER}\nexport const i18nMap = {json.dumps(i18n_map_dict, indent=2)}"
+    )
 
 # Rewrite to format the keys to not have quotes.
 with open(frontend_types_dir / "i18n-map.ts", encoding="utf-8", mode="r") as f:
