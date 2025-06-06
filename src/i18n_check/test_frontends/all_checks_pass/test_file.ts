eddadf4d84baa0dbbc6c3b7d0e77101bfe15c83d
Omar Agiez <omaragiez3@gmail.com>
2025-06-06 07:09:49 +0300

    add quotes to i18n keys to meet invalid_keys regex pattern

--- src/i18n_check/test_frontends/all_checks_pass/test_file.ts
+++ src/i18n_check/test_frontends/all_checks_pass/test_file.ts
@@ -1,3 +1,3 @@
 // This file contains the following i18n keys:
-//   - i18n._global.hello_global
-//   - i18n.test_file.hello_test_file
+//   - "i18n._global.hello_global"
+//   - "i18n.test_file.hello_test_file"
