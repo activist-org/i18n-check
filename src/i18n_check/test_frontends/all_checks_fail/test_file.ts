30b479b0e554c4bc7db72b7a6fc1add5f314e55f
Omar Agiez <omaragiez3@gmail.com>
2025-08-25 21:56:26 +0300

    #50 Add test key for repeat-value suggestion

--- src/i18n_check/test_frontends/all_checks_fail/test_file.ts
+++ src/i18n_check/test_frontends/all_checks_fail/test_file.ts
@@ -1,6 +1,8 @@
 // This file contains the following i18n keys:
 //   - "i18n._global.hello_global"
 //   - "i18n._global.hello_global_repeat_value"
+//   - "i18n.hello_global_in_single_file_repeat_value"
+//   - "i18n.hello_global_in_multiple_files"
 //   - "i18n._global.repeat_key"
 //   - "i18n.test_file.form_button_aria_label"
 //   - i18n.test_file.hello_test_file  // no quotes or backticks
