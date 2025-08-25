12eec8e79086a287df35822cdc386616bf9860d0
Omkar Sathish <omkarsathish06@gmail.com>
2025-08-25 12:23:47 +0530

    test file changes

--- src/i18n_check/test_frontends/all_checks_fail/test_file.ts
+++ src/i18n_check/test_frontends/all_checks_fail/test_file.ts
@@ -3,9 +3,11 @@
 //   - "i18n._global.hello_global_repeat_value"
 //   - "i18n._global.repeat_key"
 //   - "i18n.test_file.form_button_aria_label"
-//   - i18n.test_file.hello_test_file  // no quotes or backticks
+//   - "i18n.test_file.hero_image_alt_text"
+//   - "i18n.test_file.logo_alt_text"
+//   - i18n.test_file.hello_test_file 
 //   - "i18n.test_file.incorrectly-formatted-key"
 //   - "i18n.test_file.nested_example"
 //   - "i18n.test_file.not_in_i18n_source_file"
-//   - "i18n.test_file.repeat_key_lower"  // lower cancels the repeat value check
+//   - "i18n.test_file.repeat_key_lower"
 //   - "i18n.wrong_identifier_path.content_reference"
--- src/i18n_check/test_frontends/all_checks_pass/test_file.ts
+++ src/i18n_check/test_frontends/all_checks_pass/test_file.ts
@@ -1,4 +1,6 @@
 // This file contains the following i18n keys:
 //   - "i18n._global.hello_global"
 //   - "i18n.test_file.form_button_aria_label"
+//   - "i18n.test_file.hero_image_alt_text"
+//   - "i18n.test_file.logo_alt_text"
 //   - "i18n.test_file.hello_test_file"
