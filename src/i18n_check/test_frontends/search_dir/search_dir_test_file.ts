d4ab98fe832422a6415e6f48af438cfb8b8cd4be
Omkar Sathish <omkarsathish06@gmail.com>
2026-03-25 18:03:47 +0530

    feat: add search_dir test frontend fixture

---
+++ src/i18n_check/test_frontends/search_dir/search_dir_test_file.ts
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This file contains the following i18n keys:
+//   - "i18n._global.hello_global"  // exists in i18n source
+//   - "i18n.search_dir_test_file.not_in_i18n_source_file"  // does not exist in i18n source
